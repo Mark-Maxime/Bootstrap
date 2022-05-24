a4a11d5e94001b6c220f547d0c3fc6ec62ffd13e
Mark-Maxime <91777952+Mark-Maxime@users.noreply.github.com>
2022-05-24 13:35:46 +0630

    Add files via upload

---
+++ For_LKA/test.py
@@ -0,0 +1,20 @@
+# x="Hello World"
+# print(x)
+
+# x=15555555555555555555555555555555
+# y=4222222222222222222222222222222222222222
+# print(x+y)
+
+
+# python->
+# java->phone app
+# javascript ->
+# php-> akk- akk2 = akk- akk2 =>
+
+
+# int
+# string
+# boolean
+# float
+
+print("Hello World")
